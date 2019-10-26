8714282b7b996ade198f6a13623d4b933aac1c66
himanshu <himanshumalasi1998@gmail.com>
2019-10-27 01:23:41 +0530

    addednewfunctionality

--- slack_bot.py
+++ slack_bot.py
@@ -1,6 +1,13 @@
 from slack import RTMClient
 from utils import fetch_reply
+import json
+import time
 
+HELP_MSG = """
+Hey! I am ScoreBot. 
+I can provide you score,result,table from all around the worlds leagues :)
+This app will show you today result and fixture.
+"""
 
 @RTMClient.run_on(event="message")
 def say_hello(**payload):
@@ -13,16 +20,63 @@ def say_hello(**payload):
         user = data['user']
         client_msg_id = data['client_msg_id']
         reply = fetch_reply(text,client_msg_id)
-        web_client.chat_postMessage(channel=channel_id,
-                                   text=reply.get('data'))
+        
+        if reply['type'] == 'playerranking':
+            data = json.loads(reply['data'])
+            string = 'Rank\tName\n'
+            for i in data:
+                string += f'{i[0]}\t{i[1]}\n'
+            web_client.chat_postMessage(channel=channel_id,
+                                       text=string)
+        if reply['type'] == 'resulttoday':
+            data = json.loads(reply['data'])
+            if data['result'] == 'success':
+                string = ''
+                for i in data['content']:
+                    match = i['match']
+                    link = i['link']
+                    string += f'{match}\t{link}\n'
+                web_client.chat_postMessage(channel=channel_id,
+                                           text=string)
+            else:
+                web_client.chat_postMessage(channel=channel_id,
+                                           text='Sorry :( ,not able to find it.')
+        if reply['type'] == 'scorer':
+            data = json.loads(reply['data'])
+            if data['result'] == 'success':
+                string = f'Name\t\tTeam\tGoals\tAssist\n'
+                for i in data['content']:
+                    string += f'{i[0]}\t{i[1]}\t{i[2]}\t{i[3]}\n'
+                web_client.chat_postMessage(channel=channel_id,
+                                           text=string)
+            else:
+                web_client.chat_postMessage(channel=channel_id,
+                                          text='Sorry :( ,not able to find it.')
+        if reply['type'] == 'points_table':
+            data = json.loads(reply['data'])
+            if data['result'] == 'success':
+                string = 'pos\tName\t\tP\tGd\tPts\n'
+                for i in data['content']:
+                    
+                    string += f'{i[0]}\t{i[1]}\t{i[2]}\t{i[3]}\t{i[4]}\n'
+                web_client.chat_postMessage(channel=channel_id,
+                                       text=string)
+            else:
+                web_client.chat_postMessage(channel=channel_id,
+                                       text='Sorry :( ,not able to find it.')
+        else:
+            web_client.chat_postMessage(channel=channel_id,
+                                        text=reply.get('data') )
 
 # Slack token for bot
-slack_token = 'xoxb-781825934963-780528215330-KFT40bsiyMYoHcALtkRTfUVb'
+slack_token = 'xoxb-781825934963-802377960161-dt6FR10wrIcwX0buQcGT4vV1'
 
-client = RTMClient(token = slack_token)
-
-client.start()
-
+client = RTMClient(token = slack_token,connect_method='rtm.start'
+)
+try:
+    client.start()
+except:
+    client.start()
 #  thread_ts = data['ts'] thread_ts=thread_ts
 #        web_client.chat_postMessage(channel=channel_id,
 #                            text=f"Hi <@{user}>!")
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import apiai
 import json
-
+import requests
+from pprint import pprint
 # api.ai client 
 APIAI_ACCESS_TOKEN = "ef4ddbe4120d45ee99b53c35afa97a62"
 ai = apiai.ApiAI(APIAI_ACCESS_TOKEN)
@@ -11,6 +12,34 @@ Hey! I am ScoreBot.
 I can provide you score,result,table from all around the worlds leagues :)
 This app will show you today result and fixture.
 """
+
+def playerRanking(params):
+    url = 'http://127.0.0.1:8000/'
+    header = {'query':'playerranking'}
+    r = requests.get(url,params=header)
+    return r.content
+
+def topScorer(params):
+    url = 'http://127.0.0.1:8000/'
+    header = {'query':'scorer',
+             'league':params.get('league')}
+    r = requests.get(url,params=header)
+    return r.content
+
+def pointsTable(params):
+    url = 'http://127.0.0.1:8000/'
+    league = params.get('league')
+    header = {'query':'pointsTable',
+             'league':params.get('league')}
+    r = requests.get(url,params=header)
+    return r.content
+def resultstoday(params):
+    url = 'http://127.0.0.1:8000/'
+    league = params.get('league')
+    header = {'query':'todayresult',
+             'league':params.get('league')}
+    r = requests.get(url,params=header)
+    return r.content
 
 def apiai_response(query, session_id):
 	"""
@@ -40,8 +69,28 @@ def fetch_reply(query, session_id):
     intent, params = parse_response(response)
     reply = {}
     print('RESPONSE')
-    print(response)
+    pprint(response)
     if response['result']['action'].startswith('smalltalk'):
         reply['type'] = 'smalltalk'
+        print(response['result']['resolvedQuery'])
+        if response['result']['resolvedQuery'] == 'help' or response['result']['resolvedQuery'] == 'HELP':
+            reply['data'] = HELP_MSG
+        else:
+            reply['data'] = response['result']['fulfillment']['speech']
+    if response['result']['action'] == 'input.unknown':
+        reply['type'] = 'unknown'
         reply['data'] = response['result']['fulfillment']['speech']
+    if intent == 'playerranking':
+        reply['type']='playerranking'
+        # make a api call
+        reply['data'] = playerRanking(params)
+    if intent == 'scorer':
+        reply['type'] = 'scorer'
+        reply['data'] = topScorer(params)
+    if intent == 'points_table':
+        reply['type'] = 'points_table'
+        reply['data'] = pointsTable(params)
+    if intent=='result_today':
+        reply['type']='resulttoday'
+        reply['data']= resultstoday(params)
     return reply
